2e5e11f656b256a8dfd56621e6a047b49b31d958
Puru <puru.bhagat@yahoo.com>
2016-11-19 21:12:17 +0800

    homeys

--- app/src/main/java/com/example/gopa2000/mobapps/SignupActivity.java
+++ app/src/main/java/com/example/gopa2000/mobapps/SignupActivity.java
@@ -1,6 +1,14 @@
 package com.example.gopa2000.mobapps;
 
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.ProgressDialog;
@@ -9,8 +17,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,12 +36,18 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class SignupActivity extends AppCompatActivity {
 
     private static final String TAG = "SignupActivity";
+    private static final int PICK_FROM_CAMERA=1;
+    private static final int PICK_FROM_FILE=2;
 
+    private Uri imageCaptureUri;
+    private ImageView mImageView;
+    Button btn_choose_image;
     private EditText nameInput;
     private EditText _addressText;
     private EditText _emailText;
@@ -57,6 +73,45 @@ public class SignupActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        final String[] items = new String[] {"From Camera", "From Gallery"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item,items);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Image");
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                if(which==0){
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File file = new File(Environment.getExternalStorageDirectory(), "tmp_avatar" + String.valueOf(System.currentTimeMillis())+".jpg");
+                    imageCaptureUri=Uri.fromFile(file);
+                    try{
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT,imageCaptureUri);
+                        intent.putExtra("return data", true);
+
+                        startActivityForResult(intent,PICK_FROM_CAMERA);
+                    } catch(Exception ex){
+                        ex.printStackTrace();
+                    }
+                    dialog.cancel();
+                }else{
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent,"Complete action using"),PICK_FROM_FILE);
+                }
+            }
+        });
+
+        final AlertDialog dialog = builder.create();
+        mImageView = (ImageView) findViewById(R.id.img_show);
+        btn_choose_image = (Button) findViewById(R.id.btn_choose_image);
+        btn_choose_image.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
 
         btn_edu_add_line=(Button)findViewById(R.id.btn_edu_addline);
         parent_layout_edu = (LinearLayout)findViewById(R.id.p_edu_layout);
@@ -113,6 +168,42 @@ public class SignupActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });*/
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode!=RESULT_OK){
+            return;
+        }
+        Bitmap bitmap = null;
+        String path = "";
+        if(requestCode==PICK_FROM_FILE){
+            imageCaptureUri=data.getData();
+            path=getRealPathFromURI(imageCaptureUri);
+            if(path==null){
+                path=imageCaptureUri.getPath();
+            }
+            if(path!=null){
+                bitmap = BitmapFactory.decodeFile(path);
+            }
+        }else{
+            path=imageCaptureUri.getPath();
+            bitmap=BitmapFactory.decodeFile(path);
+        }
+        Log.d(TAG, path);
+        mImageView.setImageBitmap(bitmap);
+    }
+
+    public String getRealPathFromURI(Uri contentURI){
+        String[] proj={MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(contentURI, proj, null, null, null);
+        if(cursor==null){
+            return null;
+        }
+        int column_index=cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
     }
 
     protected void createEditTextViewEdu() {
@@ -289,4 +380,5 @@ public class SignupActivity extends AppCompatActivity {
 
         return valid;
     }*/
+
 }
